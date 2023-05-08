470e6a7eda9c9e82aab1fac62a13e0c57f596155
raul-art <132869789+raul-art@users.noreply.github.com>
2023-05-08 23:18:23 +0900

    js途中

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputTextElemennt = document.getElementById("add-text");
+  alert(inputTextElemennt.value);
+  inputTextElemennt.value = "";
+
+  alert(inputTextElemennt.value);
+}
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
